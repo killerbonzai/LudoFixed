1fc4710186ee6c16d0a1e971521b6e573e6ba13c
Morten Joelsen <bonzai.slasher@gmail.com>
2017-03-03 22:45:29 +0100

    init

---
+++ LudoAiSDK/IAiPlugin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LudoAiSDK
+{
+    public interface IAiPlugin
+    {
+        void Play(LudoGame.LudoGame LudoBoard); // done? :) fix senere til noget interface? andet?
+    }
+}
---
+++ LudoGame/LudoGame.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LudoGame
+{
+    public class LudoGame
+    {
+        // lave interface eller noget som kan bruges af AI i plugin SDK
+    }
+}
